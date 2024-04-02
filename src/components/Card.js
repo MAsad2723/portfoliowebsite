0c8fb868a04cd511831ecce87c431ddba3820c1c
Asad <muhammadasad98980@gmail.com>
2024-04-02 08:41:31 +0500

    Project section minor changin

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,18 @@ import React from 'react'
 
 function Card(prop) {
     return (
-        <div className="card m-4 col-3 cardBox">
+        // <a href="#sectionProject" target='_blank' rel="noopener noreferrer" className='card m-4 col-3 cardBox'>
+        <a href="#sectionProject" rel="noopener noreferrer" className='card m-4 col-3 cardBox'>
             <div>
-                <img src={prop.img} style={{ height: 'inherit' }} className="card-img-top" alt="..." />
-            </div>
-            <div className="card-body d-flex flex-column justify-content-end">
-                <h5 className="card-title">{prop.heading}</h5>
-                <p className="card-text text-start">{prop.desc}</p>
-            </div>
-        </div >
+                <div>
+                    <img src={prop.img} style={{ height: 'inherit' }} className="card-img-top" alt="..." />
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">{prop.heading}</h5>
+                    <p className="card-text text-start">{prop.desc}</p>
+                </div>
+            </div >
+        </a>
     )
 }
 
