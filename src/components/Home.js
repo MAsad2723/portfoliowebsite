ec12a05c9668544d867431cbf4864f9280268a4f
Asad <muhammadasad98980@gmail.com>
2024-04-12 23:11:52 +0500

    adjusted image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <section className='container home' id='homeSection'>
             <div className="row d-flex align-items-center container" style={{ margin: 'auto' }}>
-                <div className="col-lg-8 col-md-8 col-sm-12 container-fluid text-start">
+                <div className="col-lg-8 col-md-8 col-sm-8 container-fluid text-start">
                     <div>
                         <h1>
                             HELLO, I'm <span className='yellow'>Asad M. Furqan</span>
@@ -14,8 +14,14 @@ function Home() {
                         <p className='paragraph'>A passionate software engineer crafting awesome digital experiences. From designing beautiful websites to building strong backend systems, I'm here to make your online dreams a reality! Let's create something amazing together!</p>
                     </div>
                 </div>
-                <div className='col-lg-4 col-md-4 d-none d-md-block'>
-                    <Image width={'500px'} />
+                <div className='col-4 d-none d-lg-block'>
+                    <Image width={'400px'} />
+                </div>
+                <div className='col-4 d-none d-md-block d-lg-none'>
+                    <Image width={'350px'} />
+                </div>
+                <div className='col-sm-4 d-none d-sm-block d-md-none'>
+                    <Image width={'300px'} />
                 </div>
             </div>
         </section>
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ProfilePicture from '../static/linkedinProfile.png'
 function Image(props) {
     return (
-        <div className="col-5" >
+        <div className="col-5">
             <img src={ProfilePicture} alt="" width={props.width} />
         </div>
     )
